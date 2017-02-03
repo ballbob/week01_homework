740333018ac7d39c0c0801327a9415f2cc607f2d
Robert Ball <robert.ball.1989@gmail.com>
2017-02-03 12:56:11 +0000

    create and fix add_or_remove_cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,3 +6,13 @@ def total_cash(hash)
   return hash[:admin][:total_cash]
 end
 
+def add_or_remove_cash(hash,cash)
+#We want to add or remove cash to hash[:admin][:total_cash].
+#How to reference an item's location in the equ
+  current = hash[:admin][:total_cash]
+  change = cash
+  total = ( current + change ).to_i
+  hash[:admin][:total_cash] = total
+  return hash[:admin][:total_cash]
+end
+
