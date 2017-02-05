1ebde14ef2a0cce58b415d89dc8aadddb4720f3d
Robert Ball <robert.ball.1989@gmail.com>
2017-02-05 16:39:30 +0000

    created and tested function sell_pet_to_customer

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,10 +1,13 @@
+
 def pet_shop_name(hash)
   return hash[:name]
 end
 
+
 def total_cash(hash)
   return hash[:admin][:total_cash]
 end
+
 
 def add_or_remove_cash(hash,cash)
   current = hash[:admin][:total_cash]
@@ -14,9 +17,11 @@ def add_or_remove_cash(hash,cash)
   return hash[:admin][:total_cash]
 end
 
+
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
 end
+
 
 def change_pets_sold(shop,number)
   current = (shop[:admin][:pets_sold]).to_i
@@ -26,12 +31,14 @@ def change_pets_sold(shop,number)
   return shop[:admin][:pets_sold]
 end  
 
+
 def stock_count(shop)
  #Because the pets stocked are an array, if we return the final index of the final item and add one, we will have the number of pets in the shop.
  #We could pass a test expecting 'six' by making it display something to do with the final pet's name, but 
  #nevermind, there's a function for this already.
   return shop[:pets].count
 end
+
 
 def pets_by_breed(shop,breed)
   petsofbreed = Array.new
@@ -44,6 +51,7 @@ def pets_by_breed(shop,breed)
 end
 #received help from Darren on this: I had been using .include? in shop[:pets][:breed], not realising I'd already been going through the pets hash.
 
+
 def find_pet_by_name(shop,petname)
   for pet in shop[:pets]
     if pet[:name] == petname.to_s
@@ -53,6 +61,7 @@ def find_pet_by_name(shop,petname)
   pet = nil
 end
 
+
 def remove_pet_by_name(shop,petname)
   for pet in shop[:pets]
     if pet[:name] == petname.to_s
@@ -61,22 +70,40 @@ def remove_pet_by_name(shop,petname)
   end
 end
 
+
 def add_pet_to_stock(shop,newpet)
   shop[:pets].push(newpet)
 end
+
 
 def customer_pet_count(arrayinput)
   arrayinput[:pets].count
 end
 
+
 def add_pet_to_customer(customer,newpet)
   customer[:pets].push(newpet)
 end
+
 
 def customer_can_afford_pet(customerinput,putativepet)
   if customerinput[:cash] > putativepet[:price] == true
      return true
   else
       return false
+  end
+end
+
+
+def sell_pet_to_customer(shop,newpet,customer)
+  for pet in shop[:pets]
+    if pet[:name] == newpet[:name]
+      petprice = (pet[:price])
+      customer[:cash] -= petprice
+      add_or_remove_cash(shop,petprice)
+      add_pet_to_customer(customer,newpet)
+      remove_pet_by_name(shop,newpet)
+      change_pets_sold(shop,1)
+    end
   end
 end
